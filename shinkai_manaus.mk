6373742231eba7cff7a8b5bc25a27f4cea6e8df0
Minit Raj <balluraj787080@gmail.com>
2026-09-23 17:47:19 +0530

    Update SHINKAI_MAINTAINER

--- shinkai_manaus.mk
+++ shinkai_manaus.mk
@@ -25,7 +25,7 @@ PRODUCT_MODEL := motorola edge 40 neo
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 #shinkai SHIT!-->
-SHINKAI_MAINTAINER := ur MinitxT
+SHINKAI_MAINTAINER := MinitxT
 TARGET_ENABLE_BLUR := true
 WITH_GMS := true
 TARGET_HAS_UDFPS := true
